2dc05a219e0dba85ff57919b29d570d9465e0d3f
taiwah1995 <taiwah1995@gmail.com>
2026-09-21 17:21:51 +0700

    test(2026-09-23 HV): add real Poe R5 4甲; R8 still pending

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
   'use strict';
 
   const DATA_BASE = 'data';
-  const APP_DATA_VERSION = '20260921test0923';
+  const APP_DATA_VERSION = '20260921test0923r5';
   let indexData = null;
   let wpBets = null;
   let venueFilter = 'all';
