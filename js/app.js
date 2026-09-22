89883643080d371151eeb4a454cedd380a9d9c4d
taiwah1995 <taiwah1995@gmail.com>
2026-09-22 16:08:26 +0700

    Bump APP_DATA_VERSION 20260922hv0923live

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
   'use strict';
 
   const DATA_BASE = 'data';
-  const APP_DATA_VERSION = '20260921hv0923banker';
+  const APP_DATA_VERSION = '20260922hv0923live';
   let indexData = null;
   let wpBets = null;
   let venueFilter = 'all';
